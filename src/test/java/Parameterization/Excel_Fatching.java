f2021e3f4feadde2923ce6aad8e217cbb1e27914
Rohitdany <138144996+Rohitdany@users.noreply.github.com>
2023-06-29 21:56:48 +0530

    Update Excel_Fatching.java

--- src/test/java/Parameterization/Excel_Fatching.java
+++ src/test/java/Parameterization/Excel_Fatching.java
@@ -10,6 +10,7 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class Excel_Fatching {
 	public static void main(String[] args) throws Throwable {
+		System.out.Println("I am updated from Github");
 		// open file in java readable format
 		FileInputStream fis = new FileInputStream(".\\src\\main\\resources\\ExcelFile.xlsx");
 		// create workbookfactory
